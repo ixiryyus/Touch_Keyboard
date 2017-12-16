b7fdc0cf718dddebb21624b2b83b4080a14520aa
ixiryyus <julienbonnanfant@gmail.com>
2017-12-16 21:03:31 +0100

    partie affichage fonctionnel

--- srcipt.js
+++ srcipt.js
@@ -1,106 +1,45 @@
-KEY_DOWN	= 40;
-KEY_UP		= 38;
-KEY_LEFT	= 37;
-KEY_RIGHT	= 39;
+var letter
 
-KEY_END		= 35;
-KEY_BEGIN	= 36;
-
-
-
-KEY_BACK_TAB 	= 8;
-KEY_TAB				= 9;
-KEY_SH_TAB  	= 16;
-KEY_ENTER			= 13;
-KEY_ESC				= 27;
-KEY_SPACE			= 32;
-KEY_DEL				= 46;
-
-KEY_A		= 65;
-KEY_B		= 66;
-KEY_C		= 67;
-KEY_D		= 68;
-KEY_E		= 69;
-KEY_F		= 70;
-KEY_G		= 71;
-KEY_H		= 72;
-KEY_I		= 73;
-KEY_J		= 74;
-KEY_K		= 75;
-KEY_L		= 76;
-KEY_M		= 77;
-KEY_N		= 78;
-KEY_O		= 79;
-KEY_P		= 80;
-KEY_Q		= 81;
-KEY_R		= 82;
-KEY_S		= 83;
-KEY_T		= 84;
-KEY_U		= 85;
-KEY_V		= 86;
-KEY_W		= 87;
-KEY_X		= 88;
-KEY_Y		= 89;
-KEY_Z		= 90;
-
-KEY_PF1		= 112;
-KEY_PF2		= 113;
-KEY_PF3		= 114;
-KEY_PF4		= 115;
-KEY_PF5		= 116;
-KEY_PF6		= 117;
-KEY_PF7		= 118;
-KEY_PF8		= 119;
-
-REMAP_KEY_T	= 5019;
-	
-function checkEventObj ( _event_ ){
-	// --- IE explorer
-	if ( window.event )
-		return window.event;
-	// --- Netscape and other explorers
-	else
-		return _event_;
+function Get_Random_Letter() {
+	var alphabet = ["a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "q", "s", "d", "f", "g", "h", "j", "k", "l", "m", "w", "x", "c", "v", "b", "n"];
+	var index_alphabet = Math.round(Math.random() * 25);
+	letter = alphabet[index_alphabet]
+	return letter
 }
+
+function Refresh_Game() {
 	
-function applyKey (_event_){
+	if (Count_Max_Element_In_Columns() == 10){
+	alert("Game Over")
+	location.reload()}
 	
-	// --- Retrieve event object from current web explorer
-	var winObj = checkEventObj(_event_);
-	
-	var intKeyCode = winObj.keyCode;
-	var intAltKey = winObj.altKey;
-	var intCtrlKey = winObj.ctrlKey;
-		
-	// --- Access with [ALT/CTRL+Key]
-	if (intAltKey || intCtrlKey) {
-		
-		if ( intKeyCode == KEY_RIGHT || intKeyCode == KEY_LEFT ){
-			
-			// --- Display Message
-			alert("Hello with ALT/CTRL !");
-			
-			// --- Map the keyCode in another keyCode not used
-			winObj.keyCode = intKeyCode = REMAP_KEY_T;
-			winObj.returnValue = false;
-			return false;
-		}
-		
-	}
-	// --- Access without [ALT/CTRL+Key]
 	else {
-		
-		if ( intKeyCode == KEY_RIGHT || intKeyCode == KEY_LEFT ){
-			
-			// --- Display Message
-			alert("Hello !");
-			
-			// --- Map the keyCode in another keyCode not used
-			winObj.keyCode = intKeyCode = REMAP_KEY_T;
-			winObj.returnValue = false;
-			return false;
-		}
-		
+		var container = Choose_column()
+		container.innerHTML += "<div class='Touch_Keyboard_Block_letter'><p id='Random_Letter'>" + Get_Random_Letter() + "</p></div>"
+		setTimeout(Refresh_Game, 100)
 	}
-	
 }
+
+function Choose_column() {
+	var column_array = document.getElementsByClassName("Column_Touch_Keyboard_Block")
+	return column_array[Math.round(Math.random() * 12)]
+
+}
+
+function Count_Max_Element_In_Columns() {
+	var number_Elements =0
+	var column_array = document.getElementsByClassName("Column_Touch_Keyboard_Block")
+	for (var i = 0; i < column_array.length; i++) {
+		var col=column_array[i].children ; 
+		if (col.length > number_Elements) {
+			number_Elements = col.length
+			
+		}
+
+	}
+	return number_Elements
+}
+
+Get_Random_Letter();
+
+Refresh_Game();
